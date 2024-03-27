0a58122f3af941739b9e227e49b9eafcd7122975
Daniel Marino <iamdanielmarino@gmail.com>
2024-03-27 16:04:33 -0400

    sorting?

--- historical_rates/script.js
+++ historical_rates/script.js
@@ -7,14 +7,14 @@ const html = htm.bind(h)
 
 const data = signal([
   {
-    "rate": "100.00",
-    "start": dayjs().subtract(2, 'year').subtract(0, 'day').format('MM/DD/YYYY'),
-    "end": dayjs('11-11-9999').format('MM/DD/YYYY')
-  },
-  {
     "rate": "90.00",
     "start": dayjs('08-01-2019').format('MM/DD/YYYY'),
     "end": dayjs().subtract(2, 'year').subtract(1, 'day').format('MM/DD/YYYY')
+  },
+  {
+    "rate": "100.00",
+    "start": dayjs().subtract(2, 'year').subtract(0, 'day').format('MM/DD/YYYY'),
+    "end": dayjs('11-11-9999').format('MM/DD/YYYY')
   },
   {
     "rate": "80.00",
@@ -56,7 +56,7 @@ function App() {
       </thead>
       <tbody>
         ${data.value.length > 0 ? html`
-          ${data.value.map((item, index) => {
+          ${data.value.sort((a, b) => (dayjs(a.start).isAfter(dayjs(b.end)) ? 1 : -1)).reverse().map((item, index) => {
             let diff = 0
 
             if (index > 0) {
@@ -98,14 +98,14 @@ function App() {
                       <span class="pds-color-muted">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="rgba(29, 30, 28, 0.4)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-label="Calendar icon"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg>
                       </span>
-                      <input type="text" aria-label="Start date of ${item.start === "11/11/1111" ? "forever" : item.start}" value=${item.start === "11/11/1111" ? "forever" : item.start} class="pds-flex-fill" />
+                      <input type="text" aria-label="Start date of ${item.start}" value=${item.start} class="pds-flex-fill" />
                     </div>
                     to
                     <div class="pds-flex pds-gap-xs faux-input faux-input-end">
                       <span class="pds-color-muted">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="rgba(29, 30, 28, 0.4)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-label="Calendar icon"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg>
                       </span>
-                      <input type="text" aria-label="End date of ${item.end === "11/11/9999" ? "forever" : item.end}" value=${item.end === "11/11/9999" ? "forever" : item.end} class="pds-flex-fill" />
+                      <input type="text" aria-label="End date of ${item.end}" value=${item.end} class="pds-flex-fill" />
                     </div>
                   </div>
                 </td>
