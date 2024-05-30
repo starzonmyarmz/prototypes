43b1405d49d5905a85621757b3a322bc111cb06b
Daniel Marino <starzonmyarmz@gmail.com>
2024-05-30 11:03:58 -0400

    Better dialog

--- historical_rates/v1/script.js
+++ historical_rates/v1/script.js
@@ -76,98 +76,112 @@ function removeRow(index) {
 }
 
 function App() {
-  return(html`
-    <table class="pds-table">
-      <thead>
-        <tr>
-          <th class="hourly-rate pds-text-center">Hourly Rate</th>
-          <th class="start-date pds-text-center">Start date</th>
-          <th class="end-date pds-text-center">End date</th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        ${data.value.length > 0 ? html`
-          ${data.value.map((item, index) => {
-            return(html`
-              <tr>
-                <td>
-                  <div class="faux-input">
-                    <div class="pds-flex pds-gap-xs wrap currency">
-                      <span class="pds-color-muted">$</span>
-                      <input type="number" onBlur=${(event) => updateValue('rate', index)} value=${item.rate} class="pds-flex-fill pds-text-right no-style" />
-                      <span class="pds-color-muted">USD</span>
+  return (html`
+    <dialog class="pdsf-dialog pds-dialog-open pds-dialog-md" open>
+      <h1 class="pds-dialog-title">Edit George’s billable rates</h1>
+      <table class="pds-table">
+        <thead>
+          <tr>
+            <th class="hourly-rate pds-text-center">Hourly Rate</th>
+            <th class="start-date pds-text-center">Start date</th>
+            <th class="end-date pds-text-center">End date</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          ${data.value.length > 0 ? html`
+            ${data.value.map((item, index) => {
+    return (html`
+                <tr>
+                  <td>
+                    <div class="faux-input">
+                      <div class="pds-flex pds-gap-xs wrap currency">
+                        <span class="pds-color-muted">$</span>
+                        <input type="number" onBlur=${(event) => updateValue('rate', index)} value=${item.rate} class="pds-flex-fill pds-text-right no-style" />
+                        <span class="pds-color-muted">USD</span>
+                      </div>
                     </div>
-                  </div>
-                </td>
-                <td>
-                  ${data.value.length === 1 ? html`
-                    <div class="pds-text-center pds-color-muted">—</div>
-                  ` : html`
-                    ${index === data.value.length - 1 ? html`
+                  </td>
+                  <td>
+                    ${data.value.length === 1 ? html`
                       <div class="pds-text-center pds-color-muted">—</div>
                     ` : html`
-                      <div class="pds-flex pds-gap-xs faux-input date">
-                        <span class="pds-color-muted">
-                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="rgba(29, 30, 28, 0.4)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-label="Calendar icon"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg>
-                        </span>
-                        <input type="text" onBlur=${(event) => updateValue('start', index)} value=${parseToday(item.start)} />
-                      </div>
+                      ${index === data.value.length - 1 ? html`
+                        <div class="pds-text-center pds-color-muted">—</div>
+                      ` : html`
+                        <div class="pds-flex pds-gap-xs faux-input date">
+                          <span class="pds-color-muted">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="rgba(29, 30, 28, 0.4)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-label="Calendar icon"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg>
+                          </span>
+                          <input type="text" onBlur=${(event) => updateValue('start', index)} value=${parseToday(item.start)} />
+                        </div>
+                      `}
                     `}
-                  `}
-                </td>
-                <td class="pds-text-center pds-color-muted">
-                  ${data.value.length === 1 ? html`
-                    <div class="pds-text-lg">—</div>
-                  ` : html `
-                     ${index === 0 ? html`
-                     <div class="pds-text-lg">—</div>
+                  </td>
+                  <td class="pds-text-center pds-color-muted">
+                    ${data.value.length === 1 ? html`
+                      <div class="pds-text-lg">—</div>
                     ` : html`
-                      <div class="pds-text-sm">${parseYesterday(data.value[index - 1]?.start)}</div>
+                      ${index === 0 ? html`
+                      <div class="pds-text-lg">—</div>
+                      ` : html`
+                        <div class="pds-text-sm">${parseYesterday(data.value[index - 1]?.start)}</div>
+                      `}
                     `}
-                  `}
-                </td>
-                <td>
-                  <button onClick=${() => removeRow(index)} class="pds-button pds-button-icon pds-button-xs">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-label="X icon"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
-                  </button>
-                </td>
-              </tr>
-            `)}
-          )}
-        ` : html`
-        `}
-        <tr class="form">
-          <td>
-            <div class="faux-input">
-              <div class="pds-flex pds-gap-xs wrap currency">
-                <span class="pds-color-muted">$</span>
-                <input type="number" value=${newRate.value} onInput=${(event) => { newRate.value = event.target.value }} class="pds-flex-fill pds-text-right no-style" />
-                <span class="pds-color-muted">USD</span>
+                  </td>
+                  <td>
+                    <button onClick=${() => removeRow(index)} class="pds-button pds-button-icon pds-button-xs">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-label="X icon"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
+                    </button>
+                  </td>
+                </tr>
+              `)
+  }
+  )}
+          ` : html`
+            <tr>
+              <td colspan="100" class="pds-py-lg">
+                You don’t have any rates!
+              </td>
+            </tr>
+          `}
+          <tr class="spacer"><td colspan="100"></td></tr>
+          <tr class="form">
+            <td>
+              <div class="faux-input">
+                <div class="pds-flex pds-gap-xs wrap currency">
+                  <span class="pds-color-muted">$</span>
+                  <input type="number" value=${newRate.value} onInput=${(event) => { newRate.value = event.target.value }} class="pds-flex-fill pds-text-right no-style" />
+                  <span class="pds-color-muted">USD</span>
+                </div>
               </div>
-            </div>
-          </td>
-          <td>
-            ${data.value.length > 0 ? html`
-              <div class="pds-flex pds-gap-xs faux-input date">
-                <span class="pds-color-muted">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="rgba(29, 30, 28, 0.4)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-label="Calendar icon"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg>
-                </span>
-                <input type="text" value=${newStart.value} onInput=${(event) => { newStart.value = event.target.value }} />
-              </div>
-            ` : html`
-              <div class="pds-text-center pds-color-muted">—</div>
-            `}
-          </td>
-          <td class="pds-text-center pds-color-muted">
-            ${data.value.length > 0 ? '' : '—'}
-          </td>
-          <td>
-            <button onClick=${validateRow} class="pds-button pds-button-sm">Add</button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+            </td>
+            <td>
+              ${data.value.length > 0 ? html`
+                <div class="pds-flex pds-gap-xs faux-input date">
+                  <span class="pds-color-muted">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="rgba(29, 30, 28, 0.4)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-label="Calendar icon"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg>
+                  </span>
+                  <input type="text" value=${newStart.value} onInput=${(event) => { newStart.value = event.target.value }} />
+                </div>
+              ` : html`
+                <div class="pds-text-center pds-color-muted">—</div>
+              `}
+            </td>
+            <td class="pds-text-center pds-color-muted">
+              ${data.value.length > 0 ? '' : '—'}
+            </td>
+            <td>
+              <button onClick=${validateRow} class="pds-button pds-button-sm">Add</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <div class="pds-mt-lg pds-flex pds-gap-xs">
+        <button class="pds-button pds-button-primary">Save billable rates</button>
+        <button class="pds-button">Cancel</button>
+      </div>
+    </dialog>
   `)
 }
 
